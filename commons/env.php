afa09b9d8ac2e4caba2cafc8b683bfa9ef5f00df
TuyetChi26 <tuyetchi261@gmail.com>
2025-03-28 23:14:13 +0700

    tao mo hinh MVC

--- commons/env.php
+++ commons/env.php
@@ -3,13 +3,12 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost:81/duan1-pawpaw/duan1-pawpaw/');
-define('BASE_URL_ADMIN'       , 'http://localhost:81/duan1-pawpaw/duan1-pawpaw/admin/');
+define('BASE_URL'       , 'http://localhost:81/agile/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'duan1-pawpaw');  // Tên database
+define('DB_NAME'    , 'agile');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
--- index.php
+++ index.php
@@ -11,18 +11,10 @@ require_once './commons/env.php'; // Khai báo biến môi trường
 require_once './commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
-require_once './controllers/auth/registerController.php';
 require_once './controllers/homeController.php';
-require_once './controllers/bannerController.php';
-require_once './controllers/danhMucSanPhamController.php';
-require_once './controllers/order/checkoutController.php';
 
 // Require toàn bộ file Models
-require_once './models/auth/registerModel.php';
 require_once './models/homeModel.php';
-require_once './models/bannerModel.php';
-require_once './models/danhMucSanPhamModel.php';
-require_once './models/order/checkoutModel.php';
 
 
 
@@ -30,22 +22,11 @@ require_once './models/order/checkoutModel.php';
 $act = $_GET['act'] ?? '/';
 
 // mọi yêu cầu POST có thể được xử lý ngay lập tức khi handleRequest() được gọi.
-$controller =  new registerController();
 $controller->handleRequest();
 match ($act) {
 
     
     '/' => (new homeController()) -> home(),
-    'category' => (new danhMucSanPhamController()) -> productByCategorys(),
-    'banners' => (new bannerController())->banners(),
-
-    'registers' => (new registerController()) -> registers(),
-    'comfirm_registers' => (new registerController()) -> comfirm_registers(),
-    'logins' => (new registerController()) -> logins(),
-    'logout' => (new registerController())->logout(),
-
-    // thanh toán
-    'checkout' => (new checkoutController()) ->checkout(),
 
     
 
